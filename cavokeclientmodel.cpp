6baa599cdac88576757de97a0138a3d3a44b43ff
Mark <markthehopeful@gmail.com>
2022-02-05 19:51:35 +0300

    unnecessary change reverted, newline added

--- cavokeclientmodel.cpp
+++ cavokeclientmodel.cpp
@@ -13,6 +13,7 @@ void CavokeClientModel::loadQmlGame(const QString &gameName) {
     // TODO: change to url loading from cache, not resources
     // TODO: delete resource.qrc
     auto* gameModel = new CavokeQmlGameModel (QUrl("qrc:/tictactoe-files/tic-tac-toe.qml"), this); // must be alive when accessed from QML
+
     qDebug() << "Starting game : " << gameName;
 
     emit startQmlApplication(gameModel);
